10c64f3d541bacf0f17f8e03d2ea915f960e33db
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-19 14:43:33 +0200

    fix: adds devDependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const base = require('@high-standards-js/base');
+const husky = require('@high-standards-js/husky');
 
 (async() => {
     await base.checkAcceptedHighStandards();
     let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
-    packageJsonOfConfig = await base.addDependency(packageJsonOfConfig, 'commitlint');
-    if (!packageJsonOfConfig.husky) packageJsonOfConfig.husky = {};
-    if (!packageJsonOfConfig.husky.hooks) packageJsonOfConfig.husky.hooks = {};
-    if (!packageJsonOfConfig.husky.hooks['commit-msg']) {
-        packageJsonOfConfig.husky.hooks['commit-msg'] = 'commitlint -E HUSKY_GIT_PARAMS';
-    }
-    
+    packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'commitlint');
+    packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, '@commitlint/config-conventional');
+    packageJsonOfConfig = husky.addHookCommand(
+        packageJsonOfConfig, 
+        'commit-msg', 
+        'commitlint -E HUSKY_GIT_PARAMS'
+    )
     base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
 })()
