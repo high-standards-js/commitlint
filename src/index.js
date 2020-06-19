5c9aa0102fb13ae1117c81ae6593f97e7738bff1
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-19 10:31:55 +0200

    save

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ const base = require('@high-standards-js/base');
     await base.checkAcceptedHighStandards();
     let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
     packageJsonOfConfig = await base.addDependency(packageJsonOfConfig, 'commitlint');
+    if (!packageJsonOfConfig.husky) packageJsonOfConfig.husky = {};
+    if (!packageJsonOfConfig.husky.hooks) packageJsonOfConfig.husky.hooks = {};
+    if (!packageJsonOfConfig.husky.hooks['commit-msg']) {
+        packageJsonOfConfig.husky.hooks['commit-msg'] = 'commitlint -E HUSKY_GIT_PARAMS';
+    }
     base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
 })()
