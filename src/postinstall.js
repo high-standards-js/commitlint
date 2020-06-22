9e2e3f83bcce0fa5beaa6db8fc342e15a620c3c4
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-22 11:56:28 +0200

    fix: bump version

---
+++ src/postinstall.js
@@ -0,0 +1,18 @@
+const base = require('@high-standards-js/base');
+const husky = require('@high-standards-js/husky');
+
+(async () => {
+  await base.checkAcceptedHighStandards();
+  let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
+  packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'commitlint');
+  packageJsonOfConfig = await base.addDevDependency(
+    packageJsonOfConfig,
+    '@commitlint/config-conventional',
+  );
+  packageJsonOfConfig = husky.addHookCommand(
+    packageJsonOfConfig,
+    'commit-msg',
+    'commitlint -E HUSKY_GIT_PARAMS',
+  );
+  base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
+})();
